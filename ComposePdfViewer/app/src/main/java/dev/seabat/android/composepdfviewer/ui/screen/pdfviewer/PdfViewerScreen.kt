10c28118647f2ce82a366ede181d55481cd03e62
seabat <ryouta.nakasnishi.86@gmail.com>
2023-09-28 00:39:14 +0900

    ビューア画面にPagerを配置

--- ComposePdfViewer/app/src/main/java/dev/seabat/android/composepdfviewer/ui/screen/pdfviewer/PdfViewerScreen.kt
+++ ComposePdfViewer/app/src/main/java/dev/seabat/android/composepdfviewer/ui/screen/pdfviewer/PdfViewerScreen.kt
@@ -1,5 +1,10 @@
 package dev.seabat.android.composepdfviewer.ui.screen.pdfviewer
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -21,6 +26,7 @@ fun PdfViewerScreen(
     )
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun PdfViewerScreenContent(
     uiState: PdfViewerUiState,
@@ -31,6 +37,21 @@ fun PdfViewerScreenContent(
             LoadingComponent()
         }
         is UiStateType.Loaded -> {
+            val pagerState = rememberPagerState(
+                initialPage = 0,
+                pageCount = {
+                    uiState.pageNo
+                }
+
+            )
+            HorizontalPager(state = pagerState) { pageNo ->
+                // Our page content
+                Text(
+                    text = "Page: $pageNo",
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+
         }
         is UiStateType.Error -> {
         }
