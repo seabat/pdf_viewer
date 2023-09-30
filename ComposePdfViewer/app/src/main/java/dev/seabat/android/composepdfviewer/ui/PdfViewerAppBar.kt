3c23793ee009d418efdfc94548173d59edd701f2
seabat <ryouta.nakasnishi.86@gmail.com>
2023-09-30 16:33:20 +0900

    ファイル追加のアイコンをヘッダーに追加する

--- ComposePdfViewer/app/src/main/java/dev/seabat/android/composepdfviewer/ui/PdfViewerAppBar.kt
+++ ComposePdfViewer/app/src/main/java/dev/seabat/android/composepdfviewer/ui/PdfViewerAppBar.kt
@@ -1,15 +1,19 @@
 package dev.seabat.android.composepdfviewer.ui
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import dev.seabat.android.composepdfviewer.ScaffoldState
 import dev.seabat.android.composepdfviewer.ui.screen.Screen
@@ -41,5 +45,15 @@ fun PdfViewerAppBar(
         } else {
             null
         },
+        actions = {
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    Icons.Filled.AddCircle,
+                    contentDescription = "PDFファイルを追加",
+                    modifier = Modifier.size(35.dp),
+                    tint = MaterialTheme.colorScheme.primaryContainer
+                )
+            }
+        }
     )
 }
