436345706a56f254bad4f0d9cc4389f1ef83898a
seabat <ryouta.nakasnishi.86@gmail.com>
2023-10-28 21:29:36 +0900

    dependencies の見直し

--- ComposePdfViewer/app/build.gradle.kts
+++ ComposePdfViewer/app/build.gradle.kts
@@ -60,14 +60,21 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material:1.3.1")
 
+    // Compose
+    val composeBom = platform("androidx.compose:compose-bom:2023.03.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    // Material 3
+    implementation("androidx.compose.material3:material3")
+    // Material 2
+    implementation("androidx.compose.material:material")
+
+    // Android Studio Preview support
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.activity:activity-compose:1.7.0")
     // Navigation
     implementation("androidx.navigation:navigation-compose:$navVersion")
 
@@ -77,12 +84,11 @@ dependencies {
     // Log
     implementation ("com.jakewharton.timber:timber:5.0.1")
 
+    // Test
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
 
