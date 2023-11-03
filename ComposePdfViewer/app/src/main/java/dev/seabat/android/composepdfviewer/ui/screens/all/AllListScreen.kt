7b2cc1a6b09ba56c1c136fe6d6b118e277487942
seabat <ryouta.nakasnishi.86@gmail.com>
2023-11-03 16:40:22 +0900

    PDF ファイルを import した際に All リストに反映させる

--- ComposePdfViewer/app/src/main/java/dev/seabat/android/composepdfviewer/ui/screens/all/AllListScreen.kt
+++ ComposePdfViewer/app/src/main/java/dev/seabat/android/composepdfviewer/ui/screens/all/AllListScreen.kt
@@ -31,6 +31,7 @@ fun AllListScreen(
             PdfViewerAppBar(
                 navController = navController,
                 onPdfImported = { pdf ->
+                    viewModel.reload()
                     val jsonString = PdfEntity.convertObjectToJson(pdf)
                     navController.navigate("pdf_viewer" + "/?pdf=${jsonString}")
                 }
